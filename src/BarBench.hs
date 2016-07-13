8901323ae3db684b9c541b9aabfdb9521135fec6
michaelt <what_is_it_to_do_anything@yahoo.com>
2016-07-13 19:42:06 -0400

    mark benches

--- src/BarBench.hs
+++ src/BarBench.hs
@@ -19,15 +19,15 @@ benches =
 benches' :: Benchmark
 benches' =
     let x n = Bar $ scalarFoo n :: Bar Foo F128
-    in bench "Bar" $ nf (bar . x) 0
+    in bench "Bar\'" $ nf (bar . x) 0
 
 -- {-#INLINE benches_ #-}
 
 benches_ :: Benchmark
-benches_ = bench "Bar" $ nf (bar . (\n -> Bar $ scalarFoo n :: Bar Foo F128)) 0  
+benches_ = bench "Bar_" $ nf (bar . (\n -> Bar $ scalarFoo n :: Bar Foo F128)) 0  
 -- {-#INLINE benches'' #-}
   
 benches'' :: Int64 -> Benchmark
 benches'' m =
   let x = Bar $ scalarFoo m :: Bar Foo F128
-  in bench "Bar" $ nf bar x
+  in bench "Bar\'\'" $ nf bar x
--- src/FooBench.hs
+++ src/FooBench.hs
@@ -10,18 +10,16 @@ benches =
   let x = scalarFoo 0 :: Foo F128
   in bench "Foo" $ nf foo x
   
---
-
 benches' :: Benchmark
 benches' =
   let x n = scalarFoo n :: Foo F128
-  in bench "Foo" $ nf (foo . x) 0
+  in bench "Foo\'" $ nf (foo . x) 0
   
 benches_ :: Benchmark
-benches_ = bench "Foo" $ nf (foo . (\n -> scalarFoo n :: Foo F128)) 0
+benches_ = bench "Foo_" $ nf (foo . (\n -> scalarFoo n :: Foo F128)) 0
 
 benches'' :: Int64 -> Benchmark
 benches'' m =
   let x = scalarFoo m :: Foo F128
-  in bench "Foo" $ nf foo x
+  in bench "Foo\'\'" $ nf foo x
   
